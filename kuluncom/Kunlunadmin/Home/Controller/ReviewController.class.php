17a4b1cb82d71ea8202f881e5395045c83ee264d
wtydreamt <wtydreamt@163.com>
2017-09-18 18:07:45 +0800

    msg

--- kuluncom/Kunlunadmin/Home/Controller/ReviewController.class.php
+++ kuluncom/Kunlunadmin/Home/Controller/ReviewController.class.php
@@ -32,4 +32,37 @@ class ReviewController extends CommonController {
 		} 	       	
 
     }
+
+    public function reviewlist(){
+        $data=I();
+        $Offset="10";
+        $page=isset($data['page'])?$data['page']:1;
+        if($page>1){
+            $start=($page-1)*$Offset;
+        }else{
+            $start=0;
+        }
+        $count=M("review")
+             ->count();
+        $arr=M("review")
+             ->limit($start,$Offset)
+             ->select();
+        $datainfo=array();
+        $datainfo['data']=$arr;
+        $datainfo['page']=$page;
+        $datainfo['pagenumer']=ceil($count/$Offset);
+        // print_r($datainfo);die;
+        $this->assign("reviewlist",$datainfo);
+        $this->display("reviewlist");
+    }
+
+    public function delreview(){
+    	$id=I("id");
+    	$res=M("review")->where(array("id"=>$id))->delete();
+    		if($res){
+    			$this->success("删除成功","reviewlist");
+    		}else{
+    			$this->success("删除失败","reviewlist");
+    		}    	
+    }
 }
