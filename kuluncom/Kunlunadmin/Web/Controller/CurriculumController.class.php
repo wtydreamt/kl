1f0b3976e3c882eeebb07c2a175445fb3adee64c
wtydreamt <wtydreamt@163.com>
2017-09-18 17:29:23 +0800

    de

--- kuluncom/Kunlunadmin/Web/Controller/CurriculumController.class.php
+++ kuluncom/Kunlunadmin/Web/Controller/CurriculumController.class.php
@@ -22,11 +22,10 @@ class CurriculumController extends Controller {
     public function info(){
     	$data=I();
     	$res=M("curriculum")->where(array("id"=>$data['id']))->field("u_id,name,ketime,fxrade,starttime,price,desc,kegrade,img,link")->find();
+        $evaluate=M("evaluate")->where(array("u_id"=>$res['u_id']))->select();
     	$user=M("admin_user")->where(array("u_id"=>$res['u_id']))->field("nickname")->find();
     	$lists=D("Common")->queryjoin("curriculum","kl_admin_user","u_id","u_id",array("kl_admin_user.u_id"=>$res['u_id']),"id,kl_admin_user.nickname,kl_admin_user.u_id,kl_curriculum.name,ketime,price,kl_curriculum.img",$type="all");
-    	
     	$menu=M("menu")->where(array("kid"=>$data['id']))->field("title,link,shi")->select();
-        $evaluate=M("evaluate")->where(array("u_id"=>$res['u_id']))->select();
         print_r($evaluate);die;
     	$this->assign("res",$res);
         $this->assign("evaluate",$evaluate);
